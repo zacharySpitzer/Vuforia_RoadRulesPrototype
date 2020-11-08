1bb359e20c1d674b0eeeabd40dcbb9c19c6df7c8
zacharySpitzer <45111466+zacharySpitzer@users.noreply.github.com>
2020-11-07 23:40:31 -0500

    Project push

---
+++ Assets/Scripts/Button2.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Vuforia;
+
+public class Button2 : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vbGameObject;
+    public GameObject Car1, Car2;
+    public List<GameObject> MoveToPoints;
+    public GameObject Pannel;
+    public GameObject InstructionsPannelButton;
+    public float timer = 3f;
+    bool isPressed;
+    public GameObject theScenario;
+    void Start()
+    {
+        // MoveToPoints = new List<GameObject>();
+        vbGameObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+    private void Update()
+    {
+        //    if (isPressed == true)
+        //    {
+        //        timer -= Time.deltaTime;
+
+        //    }
+        //    if (timer <= 0)
+        //    {
+        //        timer = 3;
+        //    }
+
+    }
+
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //cube.transform.Rotate(Vector3.right * Time.deltaTime); ;
+        // Debug.Log("vb Pressed");
+        InstructionsPannelButton.SetActive(false);
+        Pannel.SetActive(true);
+        Debug.Log("vb Pressed");
+        isPressed = false;
+
+
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //cube.SetActive(true);
+        Debug.Log("vb Released");
+    }
+    public void MoveObjects()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/PannelManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PannelManager : MonoBehaviour
+{
+    public GameObject instructionPannel, Button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Instructions()
+    {
+        instructionPannel.SetActive(true);
+        Button.SetActive(false);
+    }
+    public void InstructionsScene()
+    {
+        SceneManager.LoadScene(1);
+       
+    }
+    public void InstructionScreen()
+    {
+        Button.SetActive(true);
+        instructionPannel.SetActive(false);
+    }
+    public void Close()
+    {
+        Application.Quit();
+    }
+    public void ResetScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
